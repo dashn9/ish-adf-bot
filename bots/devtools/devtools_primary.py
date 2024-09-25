f94ab7ba13ce657dceda9429bd424a838e25283f
Daniel Shogbon <ishogbon@gmail.com>
2024-09-25 20:48:08 +0100

    updated devtools mobile activation

--- bots/devtools/devtools_primary.py
+++ bots/devtools/devtools_primary.py
@@ -1,5 +1,6 @@
 import base64
 import re
+import asyncio
 
 from typing import Callable, Optional, List, Dict
 from nodriver import Tab, cdp
@@ -29,9 +30,12 @@ async def activate_mobile(
         }, 
     max_touch_points=5
 ):
-    await tab.send(cdp.emulation.set_emit_touch_events_for_mouse(enabled=True))
-    await tab.send(cdp.emulation.set_touch_emulation_enabled(enabled=True, max_touch_points=max_touch_points))
     await simulate_screen(tab, device_metrics)
+    await asyncio.sleep(0.1)
+    await tab.send(cdp.emulation.set_emit_touch_events_for_mouse(enabled=True))
+    await asyncio.sleep(0.1)
+    await tab.send(cdp.emulation.set_touch_emulation_enabled(enabled=True, max_touch_points=max_touch_points))
+    await asyncio.sleep(0.1)
 
 async def listen_to_tab_creation(
         browser, tab_creation_callback: Callable[[cdp.fetch.RequestPaused], bool]
