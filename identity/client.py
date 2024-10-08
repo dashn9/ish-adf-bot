19c7e65d98cc7802a3de1dad0e8084425ea1a897
Daniel Shogbon <ishogbon@gmail.com>
2024-10-08 18:12:47 +0100

    adjust countryproxy url to residential

--- identity/client.py
+++ identity/client.py
@@ -190,10 +190,10 @@ class Identity:
                 if hasattr(self, "country_proxy_url"):
                     self.country_proxy_url = self.country_proxy_url.replace(
                         "<proxy_user>",
-                        config.DATAIMPULSE_MOBILE_PROXY_CREDENTIALS["user"],
+                        config.DATAIMPULSE_RESIDENTIAL_PROXY_CREDENTIALS["user"],
                     ).replace(
                         "<proxy_password>",
-                        config.DATAIMPULSE_MOBILE_PROXY_CREDENTIALS["password"],
+                        config.DATAIMPULSE_RESIDENTIAL_PROXY_CREDENTIALS["password"],
                     )
 
     async def auto_initiate_identity(self, method, method_value):
